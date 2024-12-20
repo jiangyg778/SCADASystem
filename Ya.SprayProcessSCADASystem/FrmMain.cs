a22a9c032b96b0c76c5610c699d2d216a2a0e33a
DESKTOP-DO9B8F8\admin <297138663@qq.com>
2024-12-20 21:22:41 +0800

    feat: 首次提交

---
+++ Ya.SprayProcessSCADASystem/FrmMain.cs
@@ -0,0 +1,10 @@
+namespace Ya.SprayProcessSCADASystem
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
