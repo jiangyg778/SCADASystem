32d1e1465e2b1b834289804f060d81c193a58fe3
DESKTOP-DO9B8F8\admin <297138663@qq.com>
2024-12-22 14:45:13 +0800

    feat: 新增脱脂工位ui

--- Ya.SprayProcessSCADASystem/Pages/PageEquipmentMonitor.Designer.cs
+++ Ya.SprayProcessSCADASystem/Pages/PageEquipmentMonitor.Designer.cs
@@ -28,59 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            userCounterButton1 = new UserCounterButton();
-            userDeviceUnitControl1 = new UserDeviceUnitControl();
+            uiTitlePanel1 = new Sunny.UI.UITitlePanel();
+            userDeviceState1 = new UserDeviceState();
+            userDeviceState2 = new UserDeviceState();
+            userAlarmState1 = new UserAlarmState();
+            userVarCurrentValue1 = new UserVarCurrentValue();
+            userVarCurrentValue2 = new UserVarCurrentValue();
+            uiTitlePanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // userCounterButton1
+            // uiTitlePanel1
             // 
-            userCounterButton1.BackColor = Color.Transparent;
-            userCounterButton1.ButtonName = "你好啊";
-            userCounterButton1.CounterButtonState = false;
-            userCounterButton1.CounterButtonSymbol = 61452;
-            userCounterButton1.FillColor = Color.Transparent;
-            userCounterButton1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            userCounterButton1.Location = new Point(214, 189);
-            userCounterButton1.MinimumSize = new Size(1, 1);
-            userCounterButton1.Name = "userCounterButton1";
-            userCounterButton1.RectColor = Color.Transparent;
-            userCounterButton1.Size = new Size(190, 90);
-            userCounterButton1.TabIndex = 0;
-            userCounterButton1.Text = "userCounterButton1";
-            userCounterButton1.TextAlignment = ContentAlignment.MiddleCenter;
-            userCounterButton1.VariableName = "";
+            uiTitlePanel1.Controls.Add(userVarCurrentValue2);
+            uiTitlePanel1.Controls.Add(userVarCurrentValue1);
+            uiTitlePanel1.Controls.Add(userAlarmState1);
+            uiTitlePanel1.Controls.Add(userDeviceState2);
+            uiTitlePanel1.Controls.Add(userDeviceState1);
+            uiTitlePanel1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            uiTitlePanel1.Location = new Point(9, 7);
+            uiTitlePanel1.Margin = new Padding(4, 5, 4, 5);
+            uiTitlePanel1.MinimumSize = new Size(1, 1);
+            uiTitlePanel1.Name = "uiTitlePanel1";
+            uiTitlePanel1.ShowText = false;
+            uiTitlePanel1.Size = new Size(1059, 167);
+            uiTitlePanel1.TabIndex = 0;
+            uiTitlePanel1.Text = "脱脂工位";
+            uiTitlePanel1.TextAlignment = ContentAlignment.MiddleCenter;
             // 
-            // userDeviceUnitControl1
+            // userDeviceState1
             // 
-            userDeviceUnitControl1.EquimentUnitName = "设备单元";
-            userDeviceUnitControl1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            userDeviceUnitControl1.Location = new Point(453, 265);
-            userDeviceUnitControl1.MinimumSize = new Size(1, 1);
-            userDeviceUnitControl1.Name = "userDeviceUnitControl1";
-            userDeviceUnitControl1.OpenVariableName = "";
-            userDeviceUnitControl1.RectColor = Color.Transparent;
-            userDeviceUnitControl1.Size = new Size(250, 60);
-            userDeviceUnitControl1.State = false;
-            userDeviceUnitControl1.TabIndex = 1;
-            userDeviceUnitControl1.Text = "userDeviceUnitControl1";
-            userDeviceUnitControl1.TextAlignment = ContentAlignment.MiddleCenter;
-            userDeviceUnitControl1.ClickEvent += userDeviceUnitControl1_ClickEvent;
+            userDeviceState1.DeviceRunName = "脱脂喷淋泵运行状态";
+            userDeviceState1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            userDeviceState1.Location = new Point(12, 36);
+            userDeviceState1.MinimumSize = new Size(1, 1);
+            userDeviceState1.Name = "userDeviceState1";
+            userDeviceState1.Size = new Size(260, 60);
+            userDeviceState1.State = false;
+            userDeviceState1.TabIndex = 0;
+            userDeviceState1.Text = "userDeviceState1";
+            userDeviceState1.TextAlignment = ContentAlignment.MiddleCenter;
+            userDeviceState1.VariableName = "脱脂喷淋泵运行状态";
+            // 
+            // userDeviceState2
+            // 
+            userDeviceState2.DeviceRunName = "脱脂排风机运行状态";
+            userDeviceState2.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            userDeviceState2.Location = new Point(385, 36);
+            userDeviceState2.MinimumSize = new Size(1, 1);
+            userDeviceState2.Name = "userDeviceState2";
+            userDeviceState2.Size = new Size(260, 60);
+            userDeviceState2.State = false;
+            userDeviceState2.TabIndex = 0;
+            userDeviceState2.Text = "userDeviceState1";
+            userDeviceState2.TextAlignment = ContentAlignment.MiddleCenter;
+            userDeviceState2.VariableName = "脱脂排风机运行状态";
+            // 
+            // userAlarmState1
+            // 
+            userAlarmState1.DeviceName = "脱脂低液位报警";
+            userAlarmState1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            userAlarmState1.Location = new Point(784, 36);
+            userAlarmState1.MinimumSize = new Size(1, 1);
+            userAlarmState1.Name = "userAlarmState1";
+            userAlarmState1.Size = new Size(250, 60);
+            userAlarmState1.State = true;
+            userAlarmState1.TabIndex = 1;
+            userAlarmState1.Text = "userAlarmState1";
+            userAlarmState1.TextAlignment = ContentAlignment.MiddleCenter;
+            userAlarmState1.VariableName = "脱脂低液位报警";
+            // 
+            // userVarCurrentValue1
+            // 
+            userVarCurrentValue1.DeviceVarName = "脱脂喷淋泵压力值";
+            userVarCurrentValue1.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            userVarCurrentValue1.Location = new Point(122, 102);
+            userVarCurrentValue1.MinimumSize = new Size(1, 1);
+            userVarCurrentValue1.Name = "userVarCurrentValue1";
+            userVarCurrentValue1.Size = new Size(414, 47);
+            userVarCurrentValue1.TabIndex = 2;
+            userVarCurrentValue1.Text = "userVarCurrentValue1";
+            userVarCurrentValue1.TextAlignment = ContentAlignment.MiddleCenter;
+            userVarCurrentValue1.Unit = "Mpa";
+            userVarCurrentValue1.VariableName = "脱脂喷淋泵压力值";
+            userVarCurrentValue1.VarValue = 0F;
+            // 
+            // userVarCurrentValue2
+            // 
+            userVarCurrentValue2.DeviceVarName = "脱脂pH值";
+            userVarCurrentValue2.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            userVarCurrentValue2.Location = new Point(542, 102);
+            userVarCurrentValue2.MinimumSize = new Size(1, 1);
+            userVarCurrentValue2.Name = "userVarCurrentValue2";
+            userVarCurrentValue2.Size = new Size(437, 47);
+            userVarCurrentValue2.TabIndex = 2;
+            userVarCurrentValue2.Text = "userVarCurrentValue1";
+            userVarCurrentValue2.TextAlignment = ContentAlignment.MiddleCenter;
+            userVarCurrentValue2.Unit = "PH";
+            userVarCurrentValue2.VariableName = "脱脂pH值";
+            userVarCurrentValue2.VarValue = 0F;
             // 
             // PageEquipmentMonitor
             // 
             AutoScaleMode = AutoScaleMode.None;
             ClientSize = new Size(1085, 549);
-            Controls.Add(userDeviceUnitControl1);
-            Controls.Add(userCounterButton1);
+            Controls.Add(uiTitlePanel1);
             Name = "PageEquipmentMonitor";
             Symbol = 57397;
             Text = "设备监控";
+            uiTitlePanel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private UserCounterButton userCounterButton1;
-        private UserDeviceUnitControl userDeviceUnitControl1;
+        private Sunny.UI.UITitlePanel uiTitlePanel1;
+        private UserDeviceState userDeviceState2;
+        private UserDeviceState userDeviceState1;
+        private UserVarCurrentValue userVarCurrentValue2;
+        private UserVarCurrentValue userVarCurrentValue1;
+        private UserAlarmState userAlarmState1;
     }
 }
